636c5b2d56bb9f18334bf31eb7180359d366bf70
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-16 21:11:03 +0800

    class TV

--- television.py
+++ television.py
@@ -2,7 +2,10 @@
 
 # create a Python Code for creating the Class named TV and a Test Driver program named TestTV that will create two objects from Class TV
 # create class named TV
-# create instance variables
+class TV:
+    def __init__(self, channel, volume, power): 
+        # create instance variables
+        
 # define functions for tv
 # create test driver program
 # call methods
