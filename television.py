b0f279a356e671ee541ce6b4fc0442b5d24418ff
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-16 21:00:29 +0800

    Pseudocode

---
+++ television.py
@@ -0,0 +1,10 @@
+# Ray Allessandra Pacis | BSCPE 1-5
+
+# create a Python Code for creating the Class named TV and a Test Driver program named TestTV that will create two objects from Class TV
+# create class named TV
+# create instance variables
+# define functions for tv
+# create test driver program
+# call methods
+# display output
+# end program
