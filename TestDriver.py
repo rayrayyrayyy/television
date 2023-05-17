7ef0152bc399a59a17a0a3f251c06d1619a5db1b
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-17 14:41:05 +0800

    remake test driver/remove buttons, reason: can't figure it out

--- TestDriver.py
+++ TestDriver.py
@@ -1,46 +1,14 @@
 # create test driver program
-# import modules and class
+# import class
 from Television import TV
-tv_1 = TV()
-tv_2 = TV()
+# define function for tv 1
+def television_1():
+    tv_1 = TV()
 
-from tkinter import * 
+# define function for tv 2
+def television_2():
+    tv_1 = TV()
 
-root = Tk()
-root.minsize(200, 200)
-root.title("TV")
+# import module
 
-# power on/off
-power = Label(root, text = "Power")
-power.pack()
-
-power_on = Button(root, text = "ON", command = TV.open_tv)
-power_on.place(x=40, y=20)
-
-power_off = Button(root, text = "OFF", command = TV.close_tv)
-power_off.place(x=120, y=20)
-
-# tv channel
-channel = Label(root, text = "Channel")
-channel.place(x=70, y=140)
-
-channel_up = Button(root, text = "<", command = TV.channel_up)
-channel_up.place(x=60, y=160)
-
-channel_down = Button(root, text = ">", command = TV.channel_down)
-channel_down.place(x=110, y=160)
-
-
-# volume
-volume_up = Button(root, text = "+", command = TV.volume_up)
-volume_up.place(x=85, y=50)
-
-volume = Label(root, text = "Volume")
-volume.place(x=70, y=75)
-
-volume_down = Button(root, text = "-", command = TV.volume_down)
-volume_down.place(x=85, y=100)
-
-# display output
 # end program
-mainloop()
