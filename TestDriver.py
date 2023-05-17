d5a5d21fcbef5e7d8987a41fe9b5edd1164aa71c
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-17 15:09:02 +0800

    volume input

--- TestDriver.py
+++ TestDriver.py
@@ -4,17 +4,19 @@ from Television import TV
 # define function for tv 1
 def television_1():
     channel = int(input("TV 1's channel is: "))
+    volume = int(input("TV 1's volume is: "))
     tv_1 = TV()
     tv_1.set_channel(channel)
-    tv_1.set_volume(15)
+    tv_1.set_volume(volume)
     tv_1.open_tv()
     
 # define function for tv 2
 def television_2():
     channel = int(input("TV 2's channel is: "))
+    volume = int(input("TV 2's volume is: "))
     tv_2 = TV()
     tv_2.set_channel(channel)
-    tv_2.set_volume(25)
+    tv_2.set_volume(volume)
     tv_2.open_tv()
 
 # import module
