30d8cb7e41fc47f40b63fda1cd6a7335f8233925
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-17 15:31:41 +0800

    tv1 output good

--- TestDriver.py
+++ TestDriver.py
@@ -6,27 +6,31 @@ def television_1():
     channel = int(input("TV 1's channel is: "))
     volume = int(input("TV 1's volume is: "))
     tv_1 = TV()
+    tv_1.open_tv()
     tv_1.set_channel(channel)
     tv_1.set_volume(volume)
-    tv_1.open_tv()
+    output_tv1 = "tv1's channel is " + str(tv_1.tv_channel()) + " and volume level is " + str(tv_1.tv_volume())
+    final_tv1.config(text = output_tv1)
     
 # define function for tv 2
 def television_2():
     channel = int(input("TV 2's channel is: "))
     volume = int(input("TV 2's volume is: "))
     tv_2 = TV()
+    tv_2.open_tv()
     tv_2.set_channel(channel)
     tv_2.set_volume(volume)
-    tv_2.open_tv()
+    
 
 # import module
 from tkinter import *
 main = Tk()
 main.title("Test Driver")
-main.minsize(200, 200)
-main.maxsize(400, 400)
-main.geometry("300x200+800+350")
+main.geometry("500x200+800+350")
 main.config(bg = "brown")
+
+final_tv1 = Label(main, text='TV 1')
+final_tv1.place(x=10, y=10)
 
 # call functions
 television_1()
